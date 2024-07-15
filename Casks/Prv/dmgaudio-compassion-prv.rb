867083e7a87c971785e5b96eeab8ada693e3cd87
resonative <174920393+resonative@users.noreply.github.com>
2024-07-14 20:34:27 -0500

    Cleaned up util function

--- Casks/Prv/dmgaudio-compassion-prv.rb
+++ Casks/Prv/dmgaudio-compassion-prv.rb
@@ -1,16 +1,13 @@
-include('opt/homebrew/Library/Taps/resonative/homebrew-proaudio/include/local_archive_url.rb')
-
 cask "dmgaudio-compassion-prv" do
-  # module Utils
-  #   def self.prv_archive_url
-  #     data = JSON.parse(File.read("#{ENV['HOMEBREW_PREFIX']}/etc/resonative/homebrew-proaudio/prvinstallerhelper.json"))
-  #     data['server'].nil? ? data['server'] = "http://not_configured.lan" : data['server']
-  #     return data['server']
-  #   end
-  # end
+  module Utils
+    def self.prv_archive_url
+      json_path = "#{ENV['HOMEBREW_PREFIX']}/etc/resonative/homebrew-proaudio/prvinstallerhelper.json"
+      data = JSON.parse(File.read(json_path))
+      data['server'] ||= "http://not_configured.lan"
+    end
+  end
   version "1.30"
   sha256 "d2bfae53e426c393a56575bb266c599f5298ba06fa6ab4d0a5dd9f86078cce15"
-
 
   url "#{Utils.prv_archive_url}/DMGAudio/CompassionMac_v#{version}WRONG.zip"
   name "DMGAudio Compassion"
