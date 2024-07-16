aae617dfc02dffc46902a5ed534df3484ca70274
resonative <174920393+resonative@users.noreply.github.com>
2024-07-16 13:30:20 -0500

    syntax cleanup

--- cmd/prvarchiveurl.rb
+++ cmd/prvarchiveurl.rb
@@ -1,57 +1,54 @@
-require 'fileutils'
-require 'json'
+# frozen_string_literal: true
+
+require "fileutils"
+require "json"
 
 module Homebrew
   module Cmd
     class PrvarchiveurlCmd < AbstractCommand
       cmd_args do
         usage_banner "`prvarchiveurl` --server 'http://some-url'"
-        description <<~EOS
+        description  <<~EOS
           Use this to configure the HTTP/HTTPS server container your installer archive. The directory listing should mirror what is cask folder.\n
           Example: http://your-server.url/CompanyName/installer_name_v130.dmg
         EOS
         flag "--url=",
-               description: "URL begin with http or https, exmaple: http://myserver.lan"
-
-        named_args max: 2
+             description: "URL begin with http or https, exmaple: http://myserver.lan"
+        named_args max: 1
       end
 
       def run
-          json_dir = "#{ENV['HOMEBREW_PREFIX']}/etc/resonative/homebrew-proaudio/"
-          json_path = "#{ENV['HOMEBREW_PREFIX']}/etc/resonative/homebrew-proaudio/prvinstallerhelper.json"
-          json_template = {
-            "server": "http://not_configured.lan"
-          }
+        json_dir = "#{ENV.fetch("HOMEBREW_PREFIX")}/etc/resonative/homebrew-proaudio/"
+        json_path = "#{ENV.fetch("HOMEBREW_PREFIX")}/etc/resonative/homebrew-proaudio/prvinstallerhelper.json"
+        json_template = { server: "http://not_configured.lan" }
 
-          # If directory does not exist, create it
-          unless File.directory?(json_dir)
-            FileUtils.mkdir_p(json_dir)
-          end
+        # If directory does not exist, create it
+        FileUtils.mkdir_p(json_dir) unless File.directory?(json_dir)
 
-          # If json file does not exist, create it
-          unless File.exist?(json_path)
-            File.open(json_path, 'w') do |file|
-              file.write (json_template.to_json)
-            end
-            puts "* prvinstallerhelper.json does not exist, created in #{json_path}"
-          end
+        # If json file does not exist, create it
+        unless File.exist?(json_path)
+          File.write(json_path, json_template.to_json)
+          puts "* prvinstallerhelper.json does not exist, created in #{json_path}"
+        end
 
-          # Read the json file
-          json_data = JSON.parse(File.read(json_path))
+        # Read the json file
+        json_data = JSON.parse(File.read(json_path))
 
-
-          # if user provides a 
-          if args.url != nil
-            if args.url =~ /\Ahttp/
-              json_data["server"] = "#{args.url}".chomp("/")
-              File.write(json_path, JSON.pretty_generate(json_data))
-              puts "* Server URL has been updated to #{json_data["server"]}"
-            else
-              puts "* It appears the URL does not start with 'http'. Please retry."
-            end
+        # if user provides an url as argument
+        if args.url
+          if args.url.start_with?("http")
+            json_data["server"] = args.url.chomp("/")
+            File.write(json_path, JSON.pretty_generate(json_data))
+            puts "* Server URL has been updated to #{json_data["server"]}"
           else
-            puts "* Server URL is #{json_data["server"]}"
+            puts "* It appears the URL does not start with 'http'. Please retry."
           end
+        else
+          puts "* Server URL is #{json_data["server"]}"
+        end
+
+        # Add test in future (ping the server, retreive http code?)
+
       end
     end
   end
