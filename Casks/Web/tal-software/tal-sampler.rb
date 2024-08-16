de2eb7e365765b06a54602d2e1a72e60f8ddf897
resonative <174920393+resonative@users.noreply.github.com>
2024-08-16 13:59:08 -0500

    first version of tal-sampler

---
+++ Casks/Web/tal-software/tal-sampler.rb
@@ -0,0 +1,19 @@
+cask "tal-sampler" do
+  version "4.6.3"
+  sha256 :no_check
+
+  url "https://tal-software.com/downloads/plugins/TAL-Sampler-installer_macos.zip"
+  name "TAL-Sampler"
+  desc "Sample player"
+  homepage "https://tal-software.com/products/tal-sampler"
+
+  livecheck do
+    cask "tal-sampler" # website regex in future
+  end
+
+  auto_updates false
+
+  pkg "TAL-Sampler-installer.pkg"
+
+  uninstall pkgutil: "com.talsoftware.pkg.TAL-Sampler-*"
+end
