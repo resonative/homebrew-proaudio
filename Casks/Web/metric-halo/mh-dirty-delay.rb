c237fe639ceb114bc5d1b3d58dc7578c707bff73
resonative <174920393+resonative@users.noreply.github.com>
2024-09-15 13:03:55 -0500

    First version of Metric Halo Production bundle plugins formulae

---
+++ Casks/Web/metric-halo/mh-dirty-delay.rb
@@ -0,0 +1,85 @@
+cask "mh-dirty-delay" do
+  version "4.0.23.187"
+  sha256 "55db69bcc39aafcad35c4d967f314b467fda82ca42a77f4ecf04801323adcc32"
+
+  url "https://mhsecure.com/installers/#{version}/MHProductionBundleInstaller.pkg"
+  name "Metric Halo MH Dirty Delay v4"
+  desc "Vintage echo and delay"
+  homepage "https://mhsecure.com/"
+
+  pkg "MHProductionBundleInstaller.pkg",
+      choices: [
+        {
+          "choiceIdentifier" => "MHChannelStrip_v4Choice",
+          "choiceAttribute"  => "selected",
+          "attributeSetting" => 0,
+        },
+        {
+          "choiceIdentifier" => "MHCharacter_v4Choice",
+          "choiceAttribute"  => "selected",
+          "attributeSetting" => 0,
+        },
+        {
+          "choiceIdentifier" => "MHDirtyDelay_v4Choice",
+          "choiceAttribute"  => "selected",
+          "attributeSetting" => 1,
+        },
+        {
+          "choiceIdentifier" => "MHHaloVerb_v4Choice",
+          "choiceAttribute"  => "selected",
+          "attributeSetting" => 0,
+        },
+        {
+          "choiceIdentifier" => "MHMultibandDynamics_v4Choice",
+          "choiceAttribute"  => "selected",
+          "attributeSetting" => 0,
+        },
+        {
+          "choiceIdentifier" => "MHMultibandExpander_v4Choice",
+          "choiceAttribute"  => "selected",
+          "attributeSetting" => 0,
+        },
+        {
+          "choiceIdentifier" => "MHPrecisionDeesser_v4Choice",
+          "choiceAttribute"  => "selected",
+          "attributeSetting" => 0,
+        },
+        {
+          "choiceIdentifier" => "MHSonicEQ_v4Choice",
+          "choiceAttribute"  => "selected",
+          "attributeSetting" => 0,
+        },
+        {
+          "choiceIdentifier" => "MHSuperGate_v4Choice",
+          "choiceAttribute"  => "selected",
+          "attributeSetting" => 0,
+        },
+        {
+          "choiceIdentifier" => "MHTransientControl_v4Choice",
+          "choiceAttribute"  => "selected",
+          "attributeSetting" => 0,
+        },
+      ]
+
+  preflight do
+    plugintype = "/tmp/com.mhlabs.installer.plugintypes"
+    # pkg postinstall script reads this file to determine what plugin format gets installed
+
+    File.open(plugintype, "w") do |file|
+      file.puts "do_aax=1\n" \
+                "do_au=1\n" \
+                "do_vst2=1\n" \
+                "do_vst3=1\n"
+    end
+  end
+
+  uninstall pkgutil: "com.mhlabs.MHProductionBundleInstaller_*.MHDirtyDelay_v4",
+            delete:  [
+              # pkgutil will delete aax
+              "/Library/Audio/Plug-Ins/Components/MHDirtyDelay_v4.component/",
+              "/Library/Audio/Plug-Ins/VST/MHDirtyDelay_v4.vst/",
+              "/Library/Audio/Plug-Ins/VST3/MHDirtyDelay_v4.vst3/",
+              "/tmp/com.mhlabs.installer.plugintypes",
+              "/tmp/MHDirtyDelay_v4_install.log",
+            ]
+end
