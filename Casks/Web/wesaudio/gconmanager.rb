c4c05d3c55ed25a43537d64ea65da63e1d8e0657
resonative <174920393+resonative@users.noreply.github.com>
2024-11-18 16:38:15 -0600

    First version Wesaudio GConManager

---
+++ Casks/Web/wesaudio/gconmanager.rb
@@ -0,0 +1,25 @@
+cask "gconmanager" do
+  version "13.0.3911"
+  sha256  "94837f3a3ec915bd7cc29de4efa5911e2657bfeeb974d43831228b9bbfb84f99"
+
+  url "https://wesaudio.com/uploads/WesAudio_MACx86x64_VST_AAX_v#{version}.dmg"
+  name "WesAudio GConManager"
+  desc "Configuration and remote control plugins for WesAudio devices"
+  homepage "https://wesaudio.com/download/"
+
+  pkg "WesAudio_MACx86x64_VST_AAX_v#{version}.pkg"
+
+  uninstall launchctl: "com.wesaudio.agent.gcon.control",
+            quit:      [
+                         "com.wesaudio.gpe.GConControl",
+                         "com.wesaudio.gpe.GConManager",
+            ],
+            pkgutil:   "com.wesaudio.*",
+            delete:    [
+              "/Applications/WesAudio/",
+              "/Library/LaunchAgents/com.wesaudio.agent.gcon.service.plist",
+              "~/Library/Saved Application State/com.wesaudio.gpe.GConManager.savedState/",
+              "~/Library/Preferences/com.wesaudio.gpe.GConControl.plist",
+              "~/Library/Preferences/com.wesaudio.gpe.GConManager.plist",
+            ]
+end
