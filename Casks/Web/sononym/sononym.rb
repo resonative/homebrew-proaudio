0830ff9cbe58e73da636406ee22638c2aeff4960
resonative <174920393+resonative@users.noreply.github.com>
2024-11-19 03:10:35 -0600

    sononym: Version update, log deletion moved from uninstall to zap stanza

--- Casks/Web/sononym/sononym.rb
+++ Casks/Web/sononym/sononym.rb
@@ -1,13 +1,13 @@
 cask "sononym" do
-  version "1.5.5"
+  version "1.5.6"
 
   on_arm do
-    sha256 "a5a8b5dcf191e83ac1324f542dba5a980a32765310bbf657488d2aec61ae0463"
+    sha256 "122802354bf18655f80043b8dab2bddcdb75c325db635df278542f699b627c4e"
 
     url "https://www.sononym.net/download/Sononym-#{version}-arm64.dmg"
   end
   on_intel do
-    sha256 "901b7ccb08eefb5f4a478d472fe5b2b244a072b6cb55e4ba350f22b752c0241d"
+    sha256 "a3b3edcb958db1f551fd27abd08a9d0b6d22375f29d94bc5d894f7d2cccf1d75"
 
     url "https://www.sononym.net/download/Sononym-#{version}.dmg"
   end
@@ -21,14 +21,12 @@ cask "sononym" do
   app "Sononym.app"
 
   uninstall quit:   "com.sononym.sononym",
-            delete: "~/Library/Saved Application State/com.sononym.sononym.savedState/",
-            trash:  [
-              "~/Library/Logs/Sononym Crawler.log",
-              "~/Library/Logs/Sononym/",
-            ]
+            delete: "~/Library/Saved Application State/com.sononym.sononym.savedState/"
 
   zap trash: [
     "~/Library/Application Support/Sononym/",
+    "~/Library/Logs/Sononym Crawler.log",
+    "~/Library/Logs/Sononym/",
     "~/Library/Preferences/com.sononym.sononym.plist",
   ]
 end
