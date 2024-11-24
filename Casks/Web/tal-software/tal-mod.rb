9debdc5eb73aad65f45095cecb7395487f74aeb1
resonative <174920393+resonative@users.noreply.github.com>
2024-11-24 02:20:40 -0600

    tal-software: revised version string, removed livecheck stanza for now

--- Casks/Web/tal-software/tal-mod.rb
+++ Casks/Web/tal-software/tal-mod.rb
@@ -1,17 +1,11 @@
 cask "tal-mod" do
-  version "1.9.8"
+  version :latest
   sha256 :no_check
 
   url "https://tal-software.com//downloads/plugins/TAL-Mod-installer_macos.zip"
   name "TAL-Mod"
   desc "Modular virtual analog synthesizer"
   homepage "https://tal-software.com/products/tal-mod"
-
-  livecheck do
-    cask "tal-mod" # website regex in future
-  end
-
-  auto_updates false
 
   pkg "TAL-Mod-installer.pkg"
 
--- Casks/Web/tal-software/tal-sampler.rb
+++ Casks/Web/tal-software/tal-sampler.rb
@@ -1,17 +1,11 @@
 cask "tal-sampler" do
-  version "4.6.3"
+  version :latest
   sha256 :no_check
 
   url "https://tal-software.com/downloads/plugins/TAL-Sampler-installer_macos.zip"
   name "TAL-Sampler"
   desc "Sample player"
   homepage "https://tal-software.com/products/tal-sampler"
-
-  livecheck do
-    cask "tal-sampler" # website regex in future
-  end
-
-  auto_updates false
 
   pkg "TAL-Sampler-installer.pkg"
 
