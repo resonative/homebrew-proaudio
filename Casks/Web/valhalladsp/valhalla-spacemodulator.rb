1674e934da229822a7644e741a94e82291c2bd8c
resonative <174920393+resonative@users.noreply.github.com>
2024-12-08 15:26:32 -0600

    valhalladsp: First version of spacemodulator and supermassive

---
+++ Casks/Web/valhalladsp/valhalla-spacemodulator.rb
@@ -0,0 +1,17 @@
+cask "valhalla-spacemodulator" do
+  version "1.2.8"
+  sha256 "12033f7f35e97e6e06e3852e42f50caff17165bc6e79696d6837d61b34858598"
+
+  url "https://valhallaproduction.s3.us-west-2.amazonaws.com/spacemod/ValhallaSpaceModulatorOSX_#{version.dots_to_underscores}.dmg",
+      verified: "valhallaproduction.s3.us-west-2.amazonaws.com/"
+  name "Valhalla Space Modulator"
+  desc "Flanger audio plugin"
+  homepage "https://valhalladsp.com/shop/modulation/valhalla-space-modulator/"
+
+  auto_updates false
+  depends_on macos: ">= :sierra"
+
+  pkg "ValhallaSpaceModulatorOSX.pkg"
+
+  uninstall pkgutil: "com.valhalladsp.SpaceModulator.pkg.*"
+end
---
+++ Casks/Web/valhalladsp/valhalla-supermassive.rb
@@ -0,0 +1,17 @@
+cask "valhalla-supermassive" do
+  version "4.0.0v4"
+  sha256 "6f64b1401cc788877bc19ad07b1d52039196b268702fdd1f458388b2f9954377"
+
+  url "https://valhallaproduction.s3.us-west-2.amazonaws.com/supermassive/ValhallaSupermassiveOSX_#{version.dots_to_underscores}.dmg",
+      verified: "valhallaproduction.s3.us-west-2.amazonaws.com/"
+  name "Valhalla Supermassive"
+  desc "Massive delays and reverbs audio plugin"
+  homepage "https://valhalladsp.com/shop/reverb/valhalla-supermassive/"
+
+  auto_updates false
+  depends_on macos: ">= :sierra"
+
+  pkg "ValhallaSupermassiveOSX.pkg"
+
+  uninstall pkgutil: "com.valhalladsp.supermassive.pkg.*"
+end
