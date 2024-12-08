1252e2a81b05e887a86d31d5d39483d11116eb3d
resonative <174920393+resonative@users.noreply.github.com>
2024-12-08 14:28:05 -0600

    monkeyc-rando: Version update, url scheme update

--- Casks/Web/monkeyc/monkeyc-rando.rb
+++ Casks/Web/monkeyc/monkeyc-rando.rb
@@ -1,24 +1,19 @@
 cask "monkeyc-rando" do
-  version "1.4.0"
-  sha256 "1eb84a124ffdf9f521355a3f17dff79b213a227a5d847a3929a3135eff89e3ff"
+  version "1.5.0"
+  sha256 "65a0cd0415c59e0f4f42856fb135a187cb04125d5b0b9ada56fcb02dd3c5f95e"
 
-  url "https://monkeyc.link/rando",
-      verified: "monkeyc.link/"
+  url "https://monkeyc.s3.eu-central-1.amazonaws.com/Rando+#{version}.zip",
+      verified: "monkeyc.s3.eu-central-1.amazonaws.com/"
   name "MonkeyC Rando"
   desc "Randomizing sampler"
   homepage "https://monkeyc.audio/pages/rando"
 
-  livecheck do
-    cask "monkeyc-rando"
-  end
-
   auto_updates false
   depends_on macos: ">= :catalina"
-  container nested: "Rando Mac 1.4.0 c5feb2b.dmg"
+  container nested: "Rando Mac #{version} f4b6f5f.dmg"
 
-  pkg "Rando Mac 1.4.0 c5feb2b.pkg"
+  pkg "Rando Mac #{version} f4b6f5f.pkg"
 
-  # need to add quit
   uninstall quit:    "com.monkeycaudio.rando",
             pkgutil: "com.monkeycaudio.rando.*"
 
