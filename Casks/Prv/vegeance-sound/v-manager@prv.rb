3cb1d1c0e26876a2d6a4082c934d0aa472b33dc0
resonative <174920393+resonative@users.noreply.github.com>
2024-12-23 19:24:40 -0600

    Converted v-manager formula into v-manager prv version (url download not working correctly)

--- Casks/Prv/vegeance-sound/v-manager@prv.rb
+++ Casks/Prv/vegeance-sound/v-manager@prv.rb
@@ -1,8 +1,22 @@
-cask "v-manager" do
+cask "v-manager@prv" do
+  module Utils
+    def self.prv_archive_url(argument = nil)
+      json_path = "#{ENV.fetch('HOMEBREW_PREFIX')}/etc/resonative/homebrew-proaudio/prvinstallerhelper.json"
+      
+      data = JSON.parse(File.read(json_path))
+      data["server"] ||= "http://not_configured.lan"
+
+      return data["server"].sub(%r{\Ahttps?://}, "") if argument == "verified"
+
+      data["server"]
+    end
+  end
+
   version "1.4.7"
   sha256 :no_check
   
-  url "https://keilwerth-audio-data-2.com/files/release/vman/#{version}/V-Manager_mac.zip"
+  url "#{Utils.prv_archive_url}/v/v-avenger/v#{version}/V-Manager_mac.zip",
+      verified: "#{Utils.prv_archive_url}/"
   name "V-Manager"
   desc "Activation manager and expansion downloader for VPS Avenger"
   homepage "https://www.vps-avenger.com/"
