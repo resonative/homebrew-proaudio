8ca3f66b2564d84fc7541099b1c78d0c7a0de761
resonative <174920393+resonative@users.noreply.github.com>
2025-01-01 14:05:45 -0600

    d16-tekturon: Typo correction

--- Casks/Web/d16-group/d16-tekturon.rb
+++ Casks/Web/d16-group/d16-tekturon.rb
@@ -2,10 +2,10 @@ cask "d16-tekturon" do
   version "1.2.2"
   sha256 "68a3f9234e485601e49e862fc4394c09582383fd87631c9ffbc66f784392dd36"
 
-  url "https://d16group.s3.amazonaws.com/installers/Tekturon/Tekturon-#{verion}.dmg",
+  url "https://d16group.s3.amazonaws.com/installers/Tekturon/Tekturon-#{version}.dmg",
       verified: "d16group.s3.amazonaws.com/"
   name "D16 Group Tekturon"
-  desc "multi-tap sequenced delay"
+  desc "Multi-tap sequenced delay"
   homepage "https://d16.pl/tekturon"
 
   auto_updates false
