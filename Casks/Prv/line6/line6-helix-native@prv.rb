83049d9390634815622d0b4a0efbb418315f4e54
resonative <174920393+resonative@users.noreply.github.com>
2025-01-03 20:47:25 -0600

    line6-helix-native@prv: Typo correction and prv_archive_url func update

--- Casks/Prv/line6/line6-helix-native@prv.rb
+++ Casks/Prv/line6/line6-helix-native@prv.rb
@@ -1,16 +1,15 @@
 cask "line6-helix-native@prv" do
-  module Utils
-    def self.prv_archive_url
-      json_path = "#{ENV['HOMEBREW_PREFIX']}/etc/resonative/homebrew-proaudio/prvinstallerhelper.json"
-      data = JSON.parse(File.read(json_path))
-      data['server'] ||= "http://not_configured.lan"
-    end
+  def self.prv_archive_url
+    json_path = "#{ENV.fetch("HOMEBREW_PREFIX")}/etc/resonative/homebrew-proaudio/prvinstallerhelper.json"
+    data = JSON.parse(File.read(json_path))
+    data["server"] ||= "http://not_configured.lan"
   end
+
   version "3.80"
   sha256 "13a6a0ac5ad6c9740d3568d6c790d6a589a984eec9e37ef143c00c32c91d1c25"
 
-  url "#{Utils.prv_archive_url}/l/line6-helix-native/v#{version}/HelixNative#{version}.dmg"
-
+  url "#{prv_archive_url}/l/line6-helix-native/v#{version}/HelixNative#{version}.dmg",
+      verified: prv_archive_url.to_s
   name "Line6 Helix Native"
   desc "Guitar amp and fx emulation"
   homepage "https://line6.com/helix/helixnative.html"
@@ -26,7 +25,7 @@ cask "line6-helix-native@prv" do
     "com.line6.component.helixvst3.package",
     "com.line6.framework.helixcore.package",
     "com.line6.helixnativecommon.package",
-  ],
+  ]
 
   zap delete: "~/Library/Application Support/Line6/Helix Native/Helix Native.prefs"
 end
