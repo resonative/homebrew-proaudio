f78dfbcc2f0893920917b80070859ac40a13bf1e
resonative <174920393+resonative@users.noreply.github.com>
2025-04-18 13:21:16 -0500

    zynaptiq-loop-editor: typo correction

--- Casks/Web/zynaptiq/zynaptiq-loop-editor.rb
+++ Casks/Web/zynaptiq/zynaptiq-loop-editor.rb
@@ -48,7 +48,7 @@ cask "zynaptiq-loop-editor" do
     puts "Please remove the folder \e[0;34m/Applications/Zynaptiq LOOP EDITOR 2/\e[0m manually."
   end
 
-  uninstall quit:    "com.zynaptiq.loopeditor2"
+  uninstall quit:    "com.zynaptiq.loopeditor2",
             pkgutil: "com.zynaptiq.pkg.loopeditor2"
 
   zap trash: [
