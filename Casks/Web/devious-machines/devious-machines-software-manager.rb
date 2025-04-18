79148991dadbe8c59fb468c49bf566ec0893fde3
resonative <174920393+resonative@users.noreply.github.com>
2025-04-18 13:18:01 -0500

    devious-machines-software-manager: version update

--- Casks/Web/devious-machines/devious-machines-software-manager.rb
+++ Casks/Web/devious-machines/devious-machines-software-manager.rb
@@ -1,6 +1,6 @@
 cask "devious-machines-software-manager" do
-  version "1.0.7"
-  sha256 "4c6743733288911c3d748ed3201b722085cb29f18e877fb7475a7ac1efc082c0"
+  version "1.1.1"
+  sha256 :no_check
 
   url "https://deviousmachines.com/dm_builds/dmsm/Devious%20Machines%20Software%20Manager%20v#{version}%20Mac.dmg"
   name "Devious Machines Software Manager"
