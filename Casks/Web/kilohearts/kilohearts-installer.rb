67e552789b082e03ab69687a78c6db8eac20558c
resonative <174920393+resonative@users.noreply.github.com>
2025-06-27 11:16:53 -0500

    kilohearts-installer: Improvement

--- Casks/Web/kilohearts/kilohearts-installer.rb
+++ Casks/Web/kilohearts/kilohearts-installer.rb
@@ -7,12 +7,21 @@ cask "kilohearts-installer" do
   desc "Plugin installer for Kilohearts products"
   homepage "https://kilohearts.com/download"
 
-  auto_updates true
+  artifact "Kilohearts Installer.app", target: "/Library/Application Support/Kilohearts/Kilohearts Installer.app"
 
-  installer manual: "Kilohearts Installer.app"
+  postflight do
+    system_command "ln",
+    args: [
+      "-s",
+      "/Library/Application Support/Kilohearts/Kilohearts Installer.app",
+      "/Applications/Kilohearts Installer.app"
+    ],
+    sudo: false
+  end
 
   uninstall quit:   "com.kiloHearts.Installer",
             delete: [
+              "/Applications/Kilohearts Installer.app",
               "/Library/Application Support/Kilohearts/index.json",
               "/Library/Application Support/Kilohearts/installer.update",
               "/Library/Application Support/Kilohearts/installer.update.meta",
