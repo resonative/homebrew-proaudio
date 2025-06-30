c3853a71685d3b1e428a26d688136b73c9491ed1
resonative <174920393+resonative@users.noreply.github.com>
2025-06-30 14:38:49 -0500

    vps-avenger@prv: Version update and code cleanup

--- Casks/Prv/vegeance-sound/vps-avenger@prv.rb
+++ Casks/Prv/vegeance-sound/vps-avenger@prv.rb
@@ -1,22 +1,15 @@
 cask "vps-avenger@prv" do
-  module Utils
-    def self.prv_archive_url(argument = nil)
-      json_path = "#{ENV.fetch('HOMEBREW_PREFIX')}/etc/resonative/homebrew-proaudio/prvinstallerhelper.json"
-      
-      data = JSON.parse(File.read(json_path))
-      data["server"] ||= "http://not_configured.lan"
-
-      return data["server"].sub(%r{\Ahttps?://}, "") if argument == "verified"
-
-      data["server"]
-    end
+  def self.prv_archive_url
+    json_path = "#{ENV.fetch("HOMEBREW_PREFIX")}/etc/resonative/homebrew-proaudio/prvinstallerhelper.json"
+    data = JSON.parse(File.read(json_path))
+    data["server"] ||= "http://not_configured.lan"
   end
 
-  version "2.3.1"
-  sha256 "82497d6b6a8c20c4d9ffd1fc087d1094146ac34c2079b17c99a38bea5da5b7eb"
+  version "2.4.1"
+  sha256 "4b91b12deca083a1b628540aa3d6b614bc3d2d005176bc81e2626c194d0281e0"
 
-  url "#{Utils.prv_archive_url}/v/vps-avenger/v#{version}/OSX_Avenger_#{version.dots_to_underscores}.dmg",
-      verified: "#{Utils.prv_archive_url}/"
+  url "#{prv_archive_url}/v/vps-avenger/v#{version}/OSX_Avenger_#{version.dots_to_underscores}.dmg",
+      verified: prv_archive_url.to_s
   name "Vengenace Sound Avenger"
   desc "Multi engine synth"
   homepage "https://vengeance-sound.com/plugins.php?sub=Vengeance%20Producer%20Suite%20Avenger%202"
