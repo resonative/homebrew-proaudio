fda33be35195b40487d6ef3cbe93c5f8d75b4840
resonative <174920393+resonative@users.noreply.github.com>
2025-06-30 09:09:57 -0500

    v-manager: Code cleanup

--- Casks/Prv/vegeance-sound/v-manager@prv.rb
+++ Casks/Prv/vegeance-sound/v-manager@prv.rb
@@ -1,22 +1,15 @@
 cask "v-manager@prv" do
-  module Utils
-    def self.prv_archive_url(argument = nil)
-      json_path = "#{ENV.fetch('HOMEBREW_PREFIX')}/etc/resonative/homebrew-proaudio/prvinstallerhelper.json"
-      
-      data = JSON.parse(File.read(json_path))
-      data["server"] ||= "http://not_configured.lan"
-
-      return data["server"].sub(%r{\Ahttps?://}, "") if argument == "verified"
-
-      data["server"]
-    end
+  def self.prv_archive_url
+    json_path = "#{ENV.fetch("HOMEBREW_PREFIX")}/etc/resonative/homebrew-proaudio/prvinstallerhelper.json"
+    data = JSON.parse(File.read(json_path))
+    data["server"] ||= "http://not_configured.lan"
   end
 
   version "1.4.7"
-  sha256 :no_check
-  
-  url "#{Utils.prv_archive_url}/v/v-manager/v#{version}/V-Manager_mac.zip",
-      verified: "#{Utils.prv_archive_url}/"
+  sha256 "ce7c712b74455bbfb8fc357887ca460c42c89f4b4b7c6d5976ae2d907398ddb7"
+
+  url "#{prv_archive_url}/v/v-manager/v#{version}/V-Manager_mac.zip",
+      verified: prv_archive_url.to_s
   name "V-Manager"
   desc "Activation manager and expansion downloader for VPS Avenger"
   homepage "https://www.vps-avenger.com/"
