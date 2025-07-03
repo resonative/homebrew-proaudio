33b0d37b2a80d5b81883b4623e3e6f4914c7f67b
resonative <174920393+resonative@users.noreply.github.com>
2025-07-03 10:09:46 -0500

    kush-hammer-mk2: First version

---
+++ Casks/Web/thehouseokush/kush-hammer-mk2.rb
@@ -0,0 +1,14 @@
+cask "kush-hammer-mk2" do
+  version "1.0.0"
+  sha256 :no_check
+
+  url "https://thedataofkush.com/installers/HammerMk2_Mac.zip",
+      verified: "thedataofkush.com/"
+  name "Kush Audio Hammer Mk2"
+  desc "Hardware EQ emulation"
+  homepage "https://thehouseofkush.com/products/hammermk2"
+
+  pkg "Hammer Mk2_#{version}.pkg"
+
+  uninstall pkgutil: "com.kushaudio.hammermk2.*"
+end
