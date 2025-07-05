ab57541590b74d12fbca5133aea862d7ad767c5e
resonative <174920393+resonative@users.noreply.github.com>
2025-07-05 10:03:47 -0500

    ew-installation-center: First version

---
+++ Casks/Web/soundonline/ew-installation-center.rb
@@ -0,0 +1,13 @@
+cask "ew-installation-center" do
+  version "1.5.1"
+  sha256  "4d551bd1ec68c8876baf572e5f84352ea85c7e46f7bcd352342523b42587d914"
+
+  url "https://software.soundsonline.com/Products/IC/IC_#{version}_Mac.zip"
+  name "East West EW Installation Center"
+  desc "Product manager/downloader for East West (Soundsonline) instruments"
+  homepage "https://www.soundsonline.com/support/updates"
+
+  pkg "EW Installation Center-#{version}.pkg"
+
+  uninstall pkgutil: "com.eastwest.pkg.installationcenter"
+end
