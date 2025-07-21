cc3c579a969cf7687c3ff532be014f96c334ec2b
resonative <174920393+resonative@users.noreply.github.com>
2025-07-21 13:58:51 -0500

    voxengo-ovc-128: New version

--- Casks/Web/voxengo/voxengo-ovc-128.rb
+++ Casks/Web/voxengo/voxengo-ovc-128.rb
@@ -1,9 +1,9 @@
 cask "voxengo-ovc-128-au-aax" do
-  version "1.13"
-  sha256 :no_check
+  version "1.14"
+  sha256 "cc390b11491545f17bd5134b957a80d6e45533d7066a664833c3c0b5d9af3c47"
 
-  url "https://www.voxengo.com/files/VoxengoOVC128_#{version.no_dots}_Mac_AU_AAX_setup.dmg"
-  name "Voxengo OVC-128 AU/AAX"
+  url "https://www.voxengo.com/files/VoxengoOVC128_#{version.no_dots}_Mac_AU_AAX_VST_setup.dmg"
+  name "Voxengo OVC-128"
   desc "Clipper with oversampling"
   homepage "https://www.voxengo.com/product/ovc128/"
 
@@ -12,9 +12,13 @@ cask "voxengo-ovc-128-au-aax" do
 
   artifact "OVC-128.component", target: "/Library/Audio/Plug-Ins/Components/OVC-128.component"
   artifact "ProTools AAX/OVC-128.aaxplugin", target: "/Library/Application Support/Avid/Audio/Plug-Ins/OVC-128.aaxplugin"
+  artifact "OVC-128.vst", target: "/Library/Audio/Plug-Ins/VST/OVC-128.vst"
+  artifact "OVC-128.vst3", target: "/Library/Audio/Plug-Ins/VST3/OVC-128.vst3"
 
   uninstall delete: [
     "/Library/Application Support/Avid/Audio/Plug-Ins/OVC-128.aaxplugin",
     "/Library/Audio/Plug-Ins/Components/OVC-128.component",
+    "/Library/Audio/Plug-Ins/VST/OVC-128.vst",
+    "/Library/Audio/Plug-Ins/VST3/OVC-128.vst3",
   ]
 end
