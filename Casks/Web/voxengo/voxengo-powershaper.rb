dc72eb2bdf4eec30f29a9ebb3d1ebed53c1d71de
resonative <174920393+resonative@users.noreply.github.com>
2025-08-11 15:29:18 -0500

    voxengo-powershaper, voxengo-water-chorus: New version

--- Casks/Web/voxengo/voxengo-powershaper.rb
+++ Casks/Web/voxengo/voxengo-powershaper.rb
@@ -1,20 +1,24 @@
-cask "voxengo-powershaper-au-aax" do
-  version "1.5"
+cask "voxengo-powershaper" do
+  version "1.6"
   sha256 :no_check
 
   url "https://www.voxengo.com/files/VoxengoPowershaper_#{version.no_dots}_Mac_AU_AAX_setup.dmg"
-  name "Voxengo Powershaper AU/AAX"
+  name "Voxengo Powershaper"
   desc "Multi-stage saturator"
   homepage "https://www.voxengo.com/product/powershaper/"
 
   auto_updates false
   depends_on macos: ">= :sierra"
 
-  artifact "Powershaper.component", target: "/Library/Audio/Plug-Ins/Components/Powershaper.component"
   artifact "ProTools AAX/Powershaper.aaxplugin", target: "/Library/Application Support/Avid/Audio/Plug-Ins/Powershaper.aaxplugin"
+  artifact "Powershaper.component", target: "/Library/Audio/Plug-Ins/Components/Powershaper.component"
+  artifact "Powershaper.vst", target: "/Library/Audio/Plug-Ins/VST/Powershaper.vst"
+  artifact "Powershaper.vst3", target: "/Library/Audio/Plug-Ins/VST3/Powershaper.vst3"
 
   uninstall delete: [
     "/Library/Application Support/Avid/Audio/Plug-Ins/Powershaper.aaxplugin",
     "/Library/Audio/Plug-Ins/Components/Powershaper.component",
+    "/Library/Audio/Plug-Ins/VST/Powershaper.vst",
+    "/Library/Audio/Plug-Ins/VST3/Powershaper.vst3",
   ]
 end
--- Casks/Web/voxengo/voxengo-water-chorus.rb
+++ Casks/Web/voxengo/voxengo-water-chorus.rb
@@ -1,5 +1,5 @@
-cask "voxengo-water-chorus-au-aax" do
-  version "1.1"
+cask "voxengo-water-chorus" do
+  version "1.2"
   sha256 :no_check
 
   url "https://www.voxengo.com/files/VoxengoWaterChorus_#{version.no_dots}_Mac_AU_AAX_setup.dmg"
@@ -10,11 +10,15 @@ cask "voxengo-water-chorus-au-aax" do
   auto_updates false
   depends_on macos: ">= :sierra"
 
-  artifact "Water Chorus.component", target: "/Library/Audio/Plug-Ins/Components/Water Chorus.component"
   artifact "ProTools AAX/Water Chorus.aaxplugin", target: "/Library/Application Support/Avid/Audio/Plug-Ins/Water Chorus.aaxplugin"
+  artifact "Water Chorus.component", target: "/Library/Audio/Plug-Ins/Components/Water Chorus.component"
+  artifact "Water Chorus.vst", target: "/Library/Audio/Plug-Ins/VST/Water Chorus.vst"
+  artifact "Water Chorus.vst3", target: "/Library/Audio/Plug-Ins/VST3/Water Chorus.vst3"
 
   uninstall delete: [
     "/Library/Application Support/Avid/Audio/Plug-Ins/Water Chorus.aaxplugin",
     "/Library/Audio/Plug-Ins/Components/Water Chorus.component",
+    "/Library/Audio/Plug-Ins/VST/Water Chorus.vst",
+    "/Library/Audio/Plug-Ins/VST3/Water Chorus.vst3",
   ]
 end
