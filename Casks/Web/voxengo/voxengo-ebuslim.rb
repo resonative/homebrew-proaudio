692269a5e79fea38afe9a00fddf7647df3f13941
resonative <174920393+resonative@users.noreply.github.com>
2025-08-11 15:24:25 -0500

    voxengo-ebuslim: New version

--- Casks/Web/voxengo/voxengo-ebuslim.rb
+++ Casks/Web/voxengo/voxengo-ebuslim.rb
@@ -1,20 +1,24 @@
-cask "voxengo-ebuslim-au-aax" do
-  version "1.10"
+cask "voxengo-ebuslim" do
+  version "1.11"
   sha256 :no_check
 
   url "https://www.voxengo.com/files/VoxengoEBusLim_#{version.no_dots}_Mac_AU_AAX_setup.dmg"
-  name "Voxengo EBusLim AU/AAX"
+  name "Voxengo EBusLim"
   desc "Low latency brickwall limiter"
   homepage "https://www.voxengo.com/product/ebuslim/"
 
   auto_updates false
   depends_on macos: ">= :sierra"
 
-  artifact "EBusLim.component", target: "/Library/Audio/Plug-Ins/Components/EBusLim.component"
   artifact "ProTools AAX/EBusLim.aaxplugin", target: "/Library/Application Support/Avid/Audio/Plug-Ins/EBusLim.aaxplugin"
+  artifact "EBusLim.component", target: "/Library/Audio/Plug-Ins/Components/EBusLim.component"
+  artifact "EBusLim.vst", target: "/Library/Audio/Plug-Ins/VST/EBusLim.vst"
+  artifact "EBusLim.vst3", target: "/Library/Audio/Plug-Ins/VST3/EBusLim.vst3"
 
   uninstall delete: [
     "/Library/Application Support/Avid/Audio/Plug-Ins/EBusLim.aaxplugin",
     "/Library/Audio/Plug-Ins/Components/EBusLim.component",
+    "/Library/Audio/Plug-Ins/VST/EBusLim.vst",
+    "/Library/Audio/Plug-Ins/VST3/EBusLim.vst3",
   ]
 end
