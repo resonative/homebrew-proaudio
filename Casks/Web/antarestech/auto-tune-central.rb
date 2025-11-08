068152c2abd09e17e944da0d497ca15d74e4c5b3
resonative <174920393+resonative@users.noreply.github.com>
2025-11-08 07:06:35 -0600

    auto-tune-central: Version update

--- Casks/Web/antarestech/auto-tune-central.rb
+++ Casks/Web/antarestech/auto-tune-central.rb
@@ -1,8 +1,8 @@
 cask "auto-tune-central" do
-  version "1.7.1"
-  sha256 "c0fa86ee8a8cfb13188d9ea868c41cd943a20c4d61fb7966e0e62a63e7c64dae"
+  version "1.8.2,6463"
+  sha256 "e2c39e9dff72977c9b126166447aaeabea1b9ef05d3a6472e5ae7cd68f81091d"
 
-  url "https://antares.sfo2.cdn.digitaloceanspaces.com/auto-tune-central/auto-update/mac/Auto_Tune_Central_#{version}_universal.dmg",
+  url "https://antares.sfo2.cdn.digitaloceanspaces.com/auto-tune-central/auto-update/mac/Auto_Tune_Central_#{version.csv.first}_universal_x#{version.csv.second}.dmg",
       verified: "antares.sfo2.cdn.digitaloceanspaces.com/"
   name "Auto-Tune Central"
   desc "Antares plugin installation manager"
