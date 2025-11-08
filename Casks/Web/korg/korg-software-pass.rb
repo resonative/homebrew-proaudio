e020ea74692760145ad56c443ff8f6a3d96e2287
resonative <174920393+resonative@users.noreply.github.com>
2025-11-08 07:50:20 -0600

    korg-software-pass: Version update

--- Casks/Web/korg/korg-software-pass.rb
+++ Casks/Web/korg/korg-software-pass.rb
@@ -1,15 +1,11 @@
 cask "korg-software-pass" do
-  version "1.3.2"
-  sha256 "a0dcdf439aad889f68e0effcd17eed69b7e1f5478994bc3533a3989619889029"
+  version "1.3.9"
+  sha256 "66f7a4d4c440c5fea4a56bf99a31b2214ecf895740722901654217fc1210b553"
 
   url "https://storage.korg.com/korgms/korg_collection/mac/KORG_Software_Pass_#{version.dots_to_underscores}.dmg"
   name "KORG Software Pass"
   desc "KORG Audio Plugin Manager"
   homepage "https://id.korg.com/static_pages/3"
-
-  livecheck do
-    cask "korg-software-pass"
-  end
 
   auto_updates true
 
