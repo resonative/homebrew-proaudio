c91ba842e31acb024194c0ce4f53f5366f47233a
resonative <174920393+resonative@users.noreply.github.com>
2025-11-08 06:59:53 -0600

    aquarius: Version update

--- Casks/Web/acustica-audio/aquarius.rb
+++ Casks/Web/acustica-audio/aquarius.rb
@@ -1,5 +1,5 @@
 cask "aquarius" do
-  version "3.0.069"
+  version "3.0.109"
   sha256 :no_check
 
   url "https://app.box.com/index.php?rm=box_download_shared_file&shared_name=ulbnd7iu184xiahx99ez2qrt5eipcw8d&file_id=f_247358825647",
@@ -10,7 +10,7 @@ cask "aquarius" do
 
   auto_updates true
 
-  pkg "Setup Aquarius #{version} macOS.pkg",
+  pkg "Setup Aquarius 3.0.109 build 1 macOS.pkg",
       choices: [
         {
           "choiceIdentifier" => "choice1",
