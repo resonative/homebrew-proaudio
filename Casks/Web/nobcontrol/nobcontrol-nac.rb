30842b0520bf73963269e27ca1fe1dfbdb8b5d54
resonative <174920393+resonative@users.noreply.github.com>
2025-11-08 08:40:26 -0600

    nobcontrol-nac: Version update

--- Casks/Web/nobcontrol/nobcontrol-nac.rb
+++ Casks/Web/nobcontrol/nobcontrol-nac.rb
@@ -1,14 +1,14 @@
 cask "nobcontrol-nac" do
-  version "0.5.13"
+  version "0.6.1"
 
   on_arm do
-    sha256 "1ec6aed4fc3e4759a784b37ef9554deb6719c213317f0e448bdff237bef5a239"
+    sha256 "71dbc5d2ddc8b2b5bda9b1e2128609f23e09ad7aba563625234fa7f0bf456b23"
 
     url "https://github.com/nobcontrol/nac/releases/download/v#{version}/nAc-#{version}-arm64.dmg",
         verified: "github.com/nobcontrol/nac/"
   end
   on_intel do
-    sha256 "cc8eef4347d4316b4d255ce41e03ee0d958f360b4b0221d1e2af3d7419e8d188"
+    sha256 "c36ccecd5f83f5f887c463b86097795db1ec5abe6e0aac218b4f9ead879bf5e1"
 
     url "https://github.com/nobcontrol/nac/releases/download/v#{version}/nAc-#{version}.dmg",
         verified: "github.com/nobcontrol/nac/"
