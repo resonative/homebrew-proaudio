9bc4ef2a7c22767bf471fca68cb248e76a64df15
resonative <174920393+resonative@users.noreply.github.com>
2025-11-09 08:03:13 -0600

    pa-installation-manager: url update

--- Casks/Web/plugin-alliance/pa-installation-manager.rb
+++ Casks/Web/plugin-alliance/pa-installation-manager.rb
@@ -2,7 +2,7 @@ cask "pa-installation-manager" do
   version "1.4.0"
   sha256 "524b79d03170653d28e835cb4d6ec2ee20a0123c0f6f9d73194cec5fd39189b8"
 
-  url "https://files.plugin-alliance.com/products/installer/PA_installation_manager_mac_#{version.dots_to_underscores}.zip"
+  url "https://pluginalliance.myshopify.com/cdn/shop/files/pa_installation_manager_mac_#{version.dots_to_underscores}.zip"
   name "Plugin Alliance Installation Manager"
   desc "Installation manager for Plugin Alliance products"
   homepage "https://www.plugin-alliance.com/en/installation-manager.html"
