5d94d2928e4e7ca11f0861228c63407786d4865e
resonative <174920393+resonative@users.noreply.github.com>
2025-11-13 01:01:34 -0600

    reason@prv: Added conflicts_with dsl reason@12prv: First version

--- Casks/Prv/reasonstudios/reason@12prv.rb
+++ Casks/Prv/reasonstudios/reason@12prv.rb
@@ -1,4 +1,4 @@
-cask "reason@prv" do
+cask "reason@12prv" do
   class << self
     define_method(:prv_archive_url) do
       json_path = "#{ENV.fetch("HOMEBREW_PREFIX")}/etc/resonative/homebrew-proaudio/prvinstallerhelper.json"
@@ -7,19 +7,19 @@ cask "reason@prv" do
     end
   end
 
-  version "13.3.3" # Reason_1333_d36-Stable-561-Mac
-  sha256 "a0bf69fd16b476df8fed5c38345c7cfbc71236d4d9ebb621e4f2204ff0dd7c5e"
+  version "12.7.4" # Reason_1274_d3-Stable-820-Mac
+  sha256 "621bf50c6d1675b97cfac7374150ac5dfc712b08be6e9997de907339075e39ac"
 
-  url "#{prv_archive_url}/r/reason/v#{version}/Reason%2013.pkg",
+  url "#{prv_archive_url}/r/reason/v#{version}/Reason%2012.pkg",
       verified: prv_archive_url.to_s
-  name "Reason 13"
+  name "Reason 12"
   desc "DAW with emphasis on instruments and effects rack"
   homepage "https://www.reasonstudios.com/reason"
 
-  auto_updates true
-  conflicts_with cask: "reason@12prv"
+  auto_updates false
+  conflicts_with cask: "reason@prv"
 
-  pkg "Reason 13.pkg"
+  pkg "Reason 12.pkg"
 
   uninstall quit:    [
               "com.reasonstudios.nautilus",
@@ -27,24 +27,25 @@ cask "reason@prv" do
               "se.propellerheads.reason",
             ],
             pkgutil: [
-              "com.reasonstudios.reason13-resources",
+              "com.reasonstudios.reason-resources",
               "se.propellerheads.ID8",
               "se.propellerheads.Orkester",
+              "se.propellerheads.Reason-12-FactorySoundBank",
+              "se.propellerheads.Reason-12-ReasonPatchCollection",
               "se.propellerheads.reason",
-              "se.propellerheads.Reason-13-FactorySoundBank",
               "se.propellerheads.reasonrackplugin",
               "se.propellerheads.reasonrackplugin-aax",
               "se.propellerheads.reasonrackplugin-au",
             ]
 
-  zap delete: [
-    "~/Library/Preferences/_Reasondata.prf",
-    "~/Library/Preferences/Reason 13 Preferences",
-    "~/Library/Preferences/Reason Plugin Dirs.prf",
-    "~/Library/Preferences/Reason_13_v1.entitlement",
-    "~/Library/Propellerhead Software/Reason/Caches/",
-    "~/Library/Propellerhead Software/Reason/Plugin Screenshots/",
-  ]
+  # zap delete: [
+  #   "~/Library/Preferences/_Reasondata.prf",
+  #   "~/Library/Preferences/Reason 13 Preferences",
+  #   "~/Library/Preferences/Reason Plugin Dirs.prf",
+  #   "~/Library/Preferences/Reason_13_v1.entitlement",
+  #   "~/Library/Propellerhead Software/Reason/Caches/",
+  #   "~/Library/Propellerhead Software/Reason/Plugin Screenshots/",
+  # ]
 
   caveats "Downloaded zip from reasonstudios.com cannot be extracted properly by brew.\n" \
           "Installer should be extracted manually with Archive Utility and store as 'Reason 13.pkg'."
