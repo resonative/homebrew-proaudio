a2b5becc9ce4e351a3993e0c9eeedfdd5aa9b179
resonative <174920393+resonative@users.noreply.github.com>
2025-11-28 11:44:30 -0600

    receivemidi, sendmidi: First version

---
+++ Casks/Web/uwyn/receivemidi.rb
@@ -0,0 +1,34 @@
+cask "receivemidi" do
+  version "1.4.4"
+  sha256  "fd1a2e8a365a39e2667b28b06db49ad829f82d219282c6fac205253534244fa5"
+
+  url "https://github.com/gbevin/ReceiveMIDI/releases/download/#{version}/receivemidi-macOS-#{version}.zip"
+  name "ReceiveMIDI"
+  desc "MIDI command-line utility"
+  homepage "https://github.com/gbevin/ReceiveMIDI/"
+
+  auto_updates false
+
+  pkg "receivemidi-macos-#{version}.pkg"
+
+  postflight do
+    system_command "ln",
+                   args: [
+                     "-s",
+                     "/usr/local/bin/receivemidi",
+                     "#{ENV.fetch("HOMEBREW_PREFIX", nil)}/bin/receivemidi",
+                   ],
+                   sudo: false
+  end
+
+  uninstall_preflight do
+    system_command "rm",
+                   args: [
+                     "-f",
+                     "#{ENV.fetch("HOMEBREW_PREFIX", nil)}/bin/receivemidi",
+                   ],
+                   sudo: false
+  end
+
+  uninstall pkgutil: "com.uwyn.receivemidi"
+end
---
+++ Casks/Web/uwyn/sendmidi.rb
@@ -0,0 +1,34 @@
+cask "sendmidi" do
+  version "1.3.1"
+  sha256  "2dc730699d3b5ed6b106b9a7fb663a142c7d2199949aea4c5a33c50d189c17a9"
+
+  url "https://github.com/gbevin/SendMIDI/releases/download/#{version}/sendmidi-macOS-#{version}.zip"
+  name "SendMIDI"
+  desc "MIDI command-line utility"
+  homepage "https://github.com/gbevin/SendMIDI/"
+
+  auto_updates false
+
+  pkg "sendmidi-macos-#{version}.pkg"
+
+  postflight do
+    system_command "ln",
+                   args: [
+                     "-s",
+                     "/usr/local/bin/sendmidi",
+                     "#{ENV.fetch("HOMEBREW_PREFIX", nil)}/bin/sendmidi",
+                   ],
+                   sudo: false
+  end
+
+  uninstall_preflight do
+    system_command "rm",
+                   args: [
+                     "-f",
+                     "#{ENV.fetch("HOMEBREW_PREFIX", nil)}/bin/sendmidi",
+                   ],
+                   sudo: false
+  end
+
+  uninstall pkgutil: "com.uwyn.sendmidi"
+end
