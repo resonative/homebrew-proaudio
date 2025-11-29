6236806e78397c5103a5eff794fdb74760b29081
resonative <174920393+resonative@users.noreply.github.com>
2025-11-29 16:53:58 -0600

    modartt-pianoteq@prv: Version update

--- Casks/Prv/modartt/modartt-pianoteq@prv.rb
+++ Casks/Prv/modartt/modartt-pianoteq@prv.rb
@@ -7,8 +7,8 @@ cask "modartt-pianoteq@prv" do
     end
   end
 
-  version "9.0.2"
-  sha256 "77fd0d85093ba5702fd258ef9c4bc05e9d50965a9f7daaa326878e40291031a3"
+  version "9.0.3"
+  sha256 "190f1c5300254790b3ff9c3d31d324549fa89267e0f5d850f64f2553652ac06e"
 
   url "#{prv_archive_url}/m/modartt-pianoteq/v#{version}/pianoteq_setup_v#{version.no_dots}.dmg",
       verified: prv_archive_url.to_s
