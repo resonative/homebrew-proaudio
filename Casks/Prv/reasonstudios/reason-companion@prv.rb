99f0c213d1d0167db2899dd7e6aa40b729c946ed
resonative <174920393+resonative@users.noreply.github.com>
2025-12-13 10:30:17 -0600

    reason-companion: Description string correction reason-companion@prv: First version

--- Casks/Prv/reasonstudios/reason-companion@prv.rb
+++ Casks/Prv/reasonstudios/reason-companion@prv.rb
@@ -1,8 +1,17 @@
-cask "reason-companion" do
-  version :latest
-  sha256 :no_check
+cask "reason-companion@prv" do
+  class << self
+    define_method(:prv_archive_url) do
+      json_path = "#{ENV.fetch("HOMEBREW_PREFIX")}/etc/resonative/homebrew-proaudio/prvinstallerhelper.json"
+      data = JSON.parse(File.read(json_path))
+      data["server"] ||= "http://not_configured.lan"
+    end
+  end
 
-  url "https://nautilus.reasonstudios.com/latest_companion_installer/mac/"
+  version "3.0.17"
+  sha256 "75b7d519651ba72529a1646590cb6681f3b8b156f5d18178e54268b94bc949d1"
+
+  url "#{prv_archive_url}/r/reason-companion/v#{version}/Reason%20Companion%20#{version}-mac.dmg",
+      verified: prv_archive_url.to_s
   name "Reason Companion"
   desc "Reason Studios software manager"
   homepage "https://www.reasonstudios.com/download-reason-companion"
