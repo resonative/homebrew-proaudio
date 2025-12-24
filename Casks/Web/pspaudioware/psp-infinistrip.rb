68a8391fd15c29cbc1e169cdab225fc06d1af5fe
resonative <174920393+resonative@users.noreply.github.com>
2025-12-24 11:07:48 -0600

    psp-infinistrip: Version update

--- Casks/Web/pspaudioware/psp-infinistrip.rb
+++ Casks/Web/pspaudioware/psp-infinistrip.rb
@@ -1,8 +1,8 @@
 cask "psp-infinistrip" do
-  version "1.3.0"
-  sha256 "04ceb5fc8ffdf23325004757f70717540896e611d026e5b1e46e0975dbcabfb1"
+  version "1.4.1,EARTH"
+  sha256 "606e290c8d8ed9aa86df66aa5a5a2b58dd65b806b906a88ea426f8a28fa959ef"
 
-  url "https://download-eu2.pspaudioware.net/PSP_InfiniStrip/OSX/PSP_InfiniStrip_#{version}_WIND_macOS.dmg",
+  url "https://download-eu2.pspaudioware.net/PSP_InfiniStrip/OSX/PSP_InfiniStrip_#{version.csv.second}_#{version.csv.first}_macOS.dmg",
       verified: "pspaudioware.net/"
   name "PSP InfiniStrip"
   desc "Zero latency channel Strip"
