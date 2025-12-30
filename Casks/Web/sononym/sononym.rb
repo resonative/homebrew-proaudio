b19012e8945ad17cfcd4f6c7bba11914284e52d9
resonative <174920393+resonative@users.noreply.github.com>
2025-12-30 13:42:44 -0600

    sononym: Version update

--- Casks/Web/sononym/sononym.rb
+++ Casks/Web/sononym/sononym.rb
@@ -1,13 +1,13 @@
 cask "sononym" do
-  version "1.6.3"
+  version "1.6.4"
 
   on_arm do
-    sha256 "6034bf995fc4501dbcf3dfea17c9c3780b6ba2409803d5e902b6d28e51d61cc0"
+    sha256 "119f319cc3aa73878ab59717d98902017f42c49fc7b817d09f38e3ec09a449d1"
 
     url "https://www.sononym.net/download/Sononym-#{version}-arm64.dmg"
   end
   on_intel do
-    sha256 "ee0b3e77420c2bbb82bf7420fcf0ea79e5c765f5e19a4b8cc59543745ebe04fe"
+    sha256 "98f90fd1c7758de0250f561950514b7c982601639e1d3bd7fa63cd49112c7f15"
 
     url "https://www.sononym.net/download/Sononym-#{version}.dmg"
   end
