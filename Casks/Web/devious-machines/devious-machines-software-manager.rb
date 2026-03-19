ca737f5617f4f882cdc07b38afd822fc0f5196a8
resonative <174920393+resonative@users.noreply.github.com>
2026-03-19 11:25:59 -0400

    devious-machines-software-manager: Version update

--- Casks/Web/devious-machines/devious-machines-software-manager.rb
+++ Casks/Web/devious-machines/devious-machines-software-manager.rb
@@ -1,6 +1,6 @@
 cask "devious-machines-software-manager" do
-  version "1.1.4"
-  sha256 "73d4d8729e96aeaa058c7a8b472b738994f1592fc11b05a43a8558787fedc3da"
+  version "1.2.9"
+  sha256 "b1592cb17d44c8e7e1f816c6e67f156c91254d89d5b339dd324dbb3586b063aa"
 
   url "https://deviousmachines.com/dm_builds/dmsm/Devious%20Machines%20Software%20Manager%20v#{version}%20Mac.dmg"
   name "Devious Machines Software Manager"
@@ -19,9 +19,12 @@ cask "devious-machines-software-manager" do
             quit:      "com.deviousmachines.downloader",
             delete:    [
               "/Library/PrivilegedHelperTools/com.deviousmachines.devioushelper",
-              "~/Library/Caches/com.deviousmachines.downloader/",
-              "~/Library/Caches/Devious Machines Software Manager/",
-              "~/Library/HTTPStorages/com.deviousmachines.downloader/",
               "~/Library/Saved Application State/com.deviousmachines.downloader.savedState",
             ]
+
+  zap delete: [
+    "~/Library/Caches/com.deviousmachines.downloader/",
+    "~/Library/Caches/Devious Machines Software Manager/",
+    "~/Library/HTTPStorages/com.deviousmachines.downloader/",
+  ]
 end
