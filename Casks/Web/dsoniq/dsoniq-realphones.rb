d8d7e4188d28f135b4158ccf4ca1440a3e6146e9
resonative <174920393+resonative@users.noreply.github.com>
2026-05-09 11:11:26 -0400

    dsoniq-realphones: Version update

--- Casks/Web/dsoniq/dsoniq-realphones.rb
+++ Casks/Web/dsoniq/dsoniq-realphones.rb
@@ -1,6 +1,6 @@
 cask "dsoniq-realphones" do
-  version "2.2.4"
-  sha256 "e288ff432821b76d925f5cb9f62d27e20836bc30982bb17c8172cc9c9c1d187a"
+  version "2.2.5"
+  sha256 "598b66b56b78457d97220adab278527d9da95b8ce1d61155256340e3ffef2dfd"
 
   url "https://download.dsoniq.com/Realphones/Realphones_installer_mac_#{version}.dmg"
   name "dSONIQ Realphones"
