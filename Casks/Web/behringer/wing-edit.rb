222f1515a4cae85a0120692bedd3f196dc9beb9e
resonative <174920393+resonative@users.noreply.github.com>
2026-06-14 08:17:21 -0500

    wing-edit: Version update

--- Casks/Web/behringer/wing-edit.rb
+++ Casks/Web/behringer/wing-edit.rb
@@ -1,8 +1,8 @@
 cask "wing-edit" do
-  version "3.2.1"
-  sha256 "5b35347069922978fabfc031c460b75b76e42162ed641374747c82a5ed1e42cb"
-
-  url "https://cdn.mediavalet.com/aunsw/musictribe/Xtl-GLnnFkyCcDbxW8v-Dg/-9lyJOI140W7m8heLGDvbA/Original/Wing-Edit_MAC_#{version}.zip",
+  version "3.3.2"
+  sha256 "e61e3600f631fb553b5e64f0e2ed5e2f933cd993673c7b04d1d82ce795db839c"
+       
+  url "https://cdn-media.empowertribe.com/e86a765cc0b0523165cc0663743752e0/WING-Edit-#{version}_Mac.zip",
       verified: "mediadl.musictribe.com/"
   name "WING-Edit"
   desc "Behringer WING Console editor"
