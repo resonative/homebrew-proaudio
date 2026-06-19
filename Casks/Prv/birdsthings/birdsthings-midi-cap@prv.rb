8b5a05bba694ab774b026fd3c22457decc5a852d
resonative <174920393+resonative@users.noreply.github.com>
2026-06-19 13:08:07 -0500

    birdsthings-midi-cap@prv: url correction

--- Casks/Prv/birdsthings/birdsthings-midi-cap@prv.rb
+++ Casks/Prv/birdsthings/birdsthings-midi-cap@prv.rb
@@ -10,7 +10,7 @@ cask "birdsthings-midi-cap@prv" do
   version "0.5.5"
   sha256 "5c8f21684a4c8193d113edac5a11cf508eddba9d69c364036d5d10facdd12be1"
 
-  url "#{prv_archive_url}/b/birdsthings-midi-cap/#{version}/leAWCvlBQRSF3aTxNxOZ_MIDI%20Cap.zip",
+  url "#{prv_archive_url}/b/birdsthings-midi-cap/v#{version}/leAWCvlBQRSF3aTxNxOZ_MIDI%20Cap.zip",
       verified: prv_archive_url.to_s  
   name "Bird's Things MIDI Cap"
   desc "Always-on MIDI recorder"
