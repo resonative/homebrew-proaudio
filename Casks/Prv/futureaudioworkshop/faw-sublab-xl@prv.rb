98b05b44af9cf4d94775bb58353ae16dcea54d4a
resonative <174920393+resonative@users.noreply.github.com>
2026-06-20 18:30:07 -0500

    fuaw-sublab-xl@prv: Removed nested dsl

--- Casks/Prv/futureaudioworkshop/faw-sublab-xl@prv.rb
+++ Casks/Prv/futureaudioworkshop/faw-sublab-xl@prv.rb
@@ -10,14 +10,13 @@ cask "faw-sublab-xl@prv" do
   version "1.0.6,100526"
   sha256 "b4a1733bf31fa9dff36f11b7442d5c402f58c6682bbd07e9cc246291bdcc5780"
 
-  url "#{prv_archive_url}/f/faw-sublab-xl/v#{version.csv.first}/SubLabXL-Installer-#{version.csv.first}-#{version.csv.second}-macOS.zip",
+  url "#{prv_archive_url}/f/faw-sublab-xl/v#{version.csv.first}/SubLabXL-Installer-#{version.csv.first}-#{version.csv.second}-macOS.dmg",
       verified: prv_archive_url.to_s
   name "Future Audio Workshop SubLab XL"
   desc "Bass synthesizer"
   homepage "https://futureaudioworkshop.com"
 
   auto_updates false
-  container nested: "SubLabXL-Installer.dmg"
 
   pkg "SubLabXL.pkg"
 
